6e0b89fd52a59246cbdca013389bd162503f6aad
5620j <sabzfooood@gmail.com>
2016-06-14 03:43:51 -0700

    Update support.lua

--- plugins/support.lua
+++ plugins/support.lua
@@ -6,7 +6,7 @@ local function callback(extra, success, result)
 end
 
 local function run(msg, matches)
-  local user = 105831687
+  local user = 189308877
 
   if matches[1] == "support" then
     user = 'user#id'..user
